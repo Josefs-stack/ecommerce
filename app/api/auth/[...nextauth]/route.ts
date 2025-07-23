e056ee3b387df674648a8939db7d198b7679c839
josef_stack <cj.devs@outlook.com>
2025-07-23 17:35:20 -0300

    criação e configuração do bd

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
+import { compare } from "bcryptjs"
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Email e Senha",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Senha", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email }
+        })
+
+        if (!user || !user.senha) return null
+
+        const isValid = await compare(credentials.password, user.senha)
+        if (!isValid) return null
+
+        return user
+      }
+    }),
+  ],
+  pages: {
+    signIn: "/login"
+  }
+})
+
+export { handler as GET, handler as POST }
