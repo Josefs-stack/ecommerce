c0cc7901d12412bdc6bb4da8d0be5284c8db6e7a
josef_stack <cj.devs@outlook.com>
2025-07-27 20:22:39 -0300

    add lightningcss

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = (globalThis as any).prisma || new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-if (process.env.NODE_ENV !== 'production') (globalThis as any).prisma = prisma
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
